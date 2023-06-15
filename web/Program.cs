985876a7ee8bf83e2967b8d53be6680bac4813b3
Hugo Vieira <hugo.vieira2002@hotmail.com>
2023-06-15 23:50:15 +0100

    request completed + added features to login + register page

--- web/Program.cs
+++ web/Program.cs
@@ -1,22 +1,17 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Blazored.SessionStorage;
+using web.Services.Http.Auth;
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromSeconds(10);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
+builder.Services.AddSession();
+builder.Services.AddBlazoredSessionStorage();
 
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient<IAuthClient, AuthClient>();
 
-builder.Services.AddScoped(sp =>
-    new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]) });
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
