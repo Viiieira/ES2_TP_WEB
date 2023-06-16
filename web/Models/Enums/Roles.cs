23b29205518ef1843b63d7cfa4639943d6797166
Gonçalo Fernandes <65426418+goncalomf@users.noreply.github.com>
2023-06-16 18:29:35 +0100

    Add some essencial models

---
+++ web/Models/Enums/Roles.cs
@@ -0,0 +1,8 @@
+namespace web.Models.Enums;
+
+public class Roles
+{
+    public static readonly string Admin = "ADMIN";
+    public static readonly string User = "USER";
+    public static readonly string UserManager = "USERMANAGER";
+}
--- web/Models/Error.cs
+++ web/Models/Error.cs
@@ -2,5 +2,12 @@ namespace web.Models;
 
 public class Error
 {
-    
+    public Error(string status, string message)
+    {
+        Status = status;
+        Message = message;
+    }
+
+    public string Status { get; set; }
+    public string Message { get; set; }
 }
