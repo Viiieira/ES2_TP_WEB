115d1e67d243a533320b0ed10cd09a240f616d82
Gonçalo Fernandes <65426418+goncalomf@users.noreply.github.com>
2023-06-16 22:56:57 +0100

    Added dependency

--- web/Program.cs
+++ web/Program.cs
@@ -1,15 +1,18 @@
 using Blazored.SessionStorage;
 using web.Services.Http.Auth;
+using web.Services.Session;
+using ISession = web.Services.Session.ISession;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
-builder.Services.AddBlazoredSessionStorage();
 
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient<IAuthClient, AuthClient>();
+builder.Services.AddScoped<ISession, Session>();
+
 
 var app = builder.Build();
 
@@ -20,7 +23,7 @@ if (!app.Environment.IsDevelopment())
         context.Request.Scheme = "https";
         return next();
     });
-    
+
     app.UseHttpsRedirection();
 }
 
