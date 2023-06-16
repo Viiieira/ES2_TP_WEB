7fa010ae672162edd96e077f49d85e18043f7a49
Gonçalo Fernandes <65426418+goncalomf@users.noreply.github.com>
2023-06-17 00:15:30 +0100

    Changes

--- web/Program.cs
+++ web/Program.cs
@@ -5,9 +5,15 @@ using ISession = web.Services.Session.ISession;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
+builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient<IAuthClient, AuthClient>();
