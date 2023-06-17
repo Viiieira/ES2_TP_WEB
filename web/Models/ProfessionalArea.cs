79ad4dee21df787d177a9e0a48872d3acebdaa39
Hugo Vieira <hugo.vieira2002@hotmail.com>
2023-06-17 12:07:03 +0100

    working request with fixed token

---
+++ web/Models/ProfessionalArea.cs
@@ -0,0 +1,13 @@
+﻿namespace web.Models;
+
+public class ProfessionalArea
+{
+    public ProfessionalArea() {}
+
+    public ProfessionalArea(string area)
+    {
+        Area = area;
+    }
+
+    public string Area { get; set; }
+}
