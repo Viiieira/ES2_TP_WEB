86ccc32063110eb0054ccbd23d11879ea9bed52e
Hugo Vieira <hugo.vieira2002@hotmail.com>
2023-06-18 18:38:55 +0100

    Printing the name of the TalentCategory instead of the Id

---
+++ web/Models/TalentCategory.cs
@@ -0,0 +1,17 @@
+﻿namespace web.Models;
+
+public class TalentCategory
+{
+    public TalentCategory()
+    {
+    }
+
+    public TalentCategory(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
